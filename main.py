e95cfecb9e64e44397d6db64bfa27a2c62601e6c
uzairfarooqui78-hash <uzairfarooqui78@gmail.com>
2026-06-08 15:01:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 from fastapi import FastAPI, UploadFile, File
 import pandas as pd
+
 from kpi_engine import calculate_kpis
 from scoring_engine import run_scoring
 
@@ -12,6 +12,7 @@ def home():
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
+
     df = pd.read_excel(file.file)
 
     kpis = calculate_kpis(df)
@@ -21,3 +22,21 @@ async def upload(file: UploadFile = File(...)):
         "kpis": kpis,
         "result": result
     }
+
+@app.post("/kpis")
+async def get_kpis(file: UploadFile = File(...)):
+
+    df = pd.read_excel(file.file)
+    kpis = calculate_kpis(df)
+
+    return {"kpis": kpis}
+
+@app.post("/score")
+async def score(file: UploadFile = File(...)):
+
+    df = pd.read_excel(file.file)
+
+    kpis = calculate_kpis(df)
+    result = run_scoring(kpis)
+
+    return {"result": result}
