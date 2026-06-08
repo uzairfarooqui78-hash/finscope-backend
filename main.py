9ea6ad5acf6bdd4c0c5171fe4be27ca7d8799885
uzairfarooqui78-hash <uzairfarooqui78@gmail.com>
2026-06-08 15:14:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
+from io import BytesIO
+
+from kpi_engine import calculate_kpis
+from scoring_engine import run_scoring
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FinScope Backend Running"}
+
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
 
     contents = await file.read()
 
-    df = pd.read_excel(contents, engine="openpyxl")
+    df = pd.read_excel(BytesIO(contents), engine="openpyxl")
 
     kpis = calculate_kpis(df)
     result = run_scoring(kpis)
